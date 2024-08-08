1dcfc0154ffe25942366a5cbb997f6c7acd17e91
MARVIN <o.silverygold@gmail.com>
2024-08-08 16:43:54 +0300

    Add recipe renderItem component

---
+++ components/recipe/RecipeCard.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const RecipeCard = ({ item }) => {
+  return (
+    <View>
+      <Text>RecipeCard</Text>
+    </View>
+  )
+}
+
+export default RecipeCard
